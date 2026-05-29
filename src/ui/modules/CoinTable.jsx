890df23ce5d1042f341b2deaef8f5a312d42fc1c
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-05-29 17:53:32 +0330

    table row component added

--- src/ui/modules/CoinTable.jsx
+++ src/ui/modules/CoinTable.jsx
@@ -1,42 +1,51 @@
 import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
-const CoinTable = ({coins}) => {
+const CoinTable = ({ coins }) => {
   return (
     <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Coin</th>
-              <th>Name</th>
-              <th>Price</th>
-              <th>24h</th>
-              <th>total volume</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              coins?.map(coin=>(
-                <tr key={coin.id}>
-                  <td>
-                    <div>
-                      <img src={coin.image} alt={coin.name} />
-                      <span>{coin.symbol.toUpperCase()}</span>
-                    </div>
-                  </td>
-                  <td>{coin.name}</td>
-                  <td>${coin.current_price.toLocaleString()}</td>
-                  <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-                  <td>{coin.total_volume.toLocaleString()}</td>
-                  <td><img src={coin.price_change_percentage_24h>0 ?chartUp:chartDown} alt="chart" /></td>
-                </tr>
+      <table>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>24h</th>
+            <th>total volume</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            coins?.map(coin => (
+                <TableRow coin={coin}/>
               ))
-            }
+          }
 
-          </tbody>
-        </table>
+        </tbody>
+      </table>
     </div>
   )
 }
 
 export default CoinTable
+
+
+const TableRow = ({coin}) => {
+  return (
+    <>
+      <tr key={coin.id}>
+        <td>
+          <div>
+            <img src={coin.image} alt={coin.name} />
+            <span>{coin.symbol.toUpperCase()}</span>
+          </div>
+        </td>
+        <td>{coin.name}</td>
+        <td>${coin.current_price.toLocaleString()}</td>
+        <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
+        <td>{coin.total_volume.toLocaleString()}</td>
+        <td><img src={coin.price_change_percentage_24h > 0 ? chartUp : chartDown} alt="chart" /></td>
+      </tr>
+    </>
+  )
+}
